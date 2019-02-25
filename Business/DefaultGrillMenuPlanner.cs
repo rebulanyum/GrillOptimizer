254f37ed04766e8e8fe5fc90caad16853df4206f
Izzet Sapkalioglu Ojalvo <izzetsapkali@hotmail.com>
2019-02-25 13:50:43 +0300

    Added exceptions

--- Business/DefaultGrillMenuPlanner.cs
+++ Business/DefaultGrillMenuPlanner.cs
@@ -29,17 +29,8 @@ namespace rebulanyum.GrillOptimizer.Business
             var grill = new Grill(GrillConfiguration);
 
             // Convert given GrillMenuModel instance into Dictionary<GrillItem, int>
-            var newMenu = new Dictionary<GrillItem, int>();
-            foreach (var grillMenuItem in menu.Items)
-            {
-                if (grillMenuItem.Quantity.Value > 0)
-                {
-                    var duration = TimeSpan.ParseExact(grillMenuItem.Duration, "hh\\:mm\\:ss", CultureInfo.InvariantCulture);
-                    var size = new Size(grillMenuItem.Width.Value, grillMenuItem.Length.Value);
-                    newMenu.Add(new GrillItem(size, duration), grillMenuItem.Quantity.Value);
-                }
-            }
-            
+            Dictionary<GrillItem, int> newMenu = ValidateAndCreateMenu(menu);
+
             while (true)
             {
                 var newMenuItems = (from newMenuItem in newMenu.Keys
@@ -51,7 +42,7 @@ namespace rebulanyum.GrillOptimizer.Business
                     break;
                 }
 
-                TimeSpan maxTimeSpan = new TimeSpan(0,0,0);
+                TimeSpan maxTimeSpan = new TimeSpan(0, 0, 0);
                 // Iterate on items with quantity > 0 and add them to to grill
                 foreach (var grillItem in newMenuItems)
                 {
@@ -71,6 +62,34 @@ namespace rebulanyum.GrillOptimizer.Business
             }
 
             return plan;
+        }
+
+        private Dictionary<GrillItem, int> ValidateAndCreateMenu(GrillMenuModel menu)
+        {
+            var newMenu = new Dictionary<GrillItem, int>();
+            foreach (var grillMenuItem in menu.Items ?? new List<GrillMenuItemModel>())
+            {
+                int quantity = grillMenuItem.Quantity.GetValueOrDefault(0);
+                if (quantity <= 0)
+                    continue;
+
+                int width = grillMenuItem.Width.GetValueOrDefault(0);
+                if (width <= 0)
+                    throw new BusinessException("The Width of the GrillMenuItemModel with the id {0} is not set.", grillMenuItem.Id.Value);
+                int length = grillMenuItem.Length.GetValueOrDefault(0); ;
+                if (length <= 0)
+                    throw new BusinessException("The Length of the GrillMenuItemModel with the id {0} is not set.", grillMenuItem.Id.Value);
+
+                if (width > GrillConfiguration.GrillSize.Width || length > GrillConfiguration.GrillSize.Height)
+                    throw new BusinessException("The Size of the GrillMenuItemModel exceeds the Grill.", grillMenuItem.Id.Value);
+
+                TimeSpan duration;
+                TimeSpan.TryParseExact(grillMenuItem.Duration, "hh\\:mm\\:ss", CultureInfo.InvariantCulture, out duration);
+                var size = new Size(width, length);
+                newMenu.Add(new GrillItem(size, duration), quantity);
+            }
+
+            return newMenu;
         }
     }
 }
---
+++ Business/Objects/BusinessException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rebulanyum.GrillOptimizer.Business.Objects
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message) : base(message)
+        { }
+
+        public BusinessException(string format, params object[] args) : base(string.Format(format, args))
+        { }
+    }
+}
