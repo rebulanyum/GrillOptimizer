eed820472e6a3e3ab5efdbc3b3686053da554343
Izzet Sapkalioglu Ojalvo <izzetsapkali@hotmail.com>
2019-02-25 14:06:47 +0300

    Added test

--- Test/DefaultGrillMenuPlannerTest.cs
+++ Test/DefaultGrillMenuPlannerTest.cs
@@ -4,7 +4,10 @@ using isolutions.GrillAssesment.Client.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using rebulanyum.GrillOptimizer.Business;
+using rebulanyum.GrillOptimizer.Business.Objects;
 using RestSharp;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace rebulanyum.GrillOptimizer.Test
@@ -30,6 +33,26 @@ namespace rebulanyum.GrillOptimizer.Test
             var planner = new DefaultGrillMenuPlanner(GrillConfiguration.Default);
             GrillMenuGrillingPlan plan = planner.Plan(result);
             Assert.AreEqual(plan.Rounds, 2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(BusinessException))]
+        //[TestCategory("TestDevelopment")]
+        public void ThrowsExceptionWhenMenuItemIsOversized()
+        {
+            var config = GrillConfiguration.Default;
+
+            var menu = new GrillMenuModel()
+            {
+                Items = new List<GrillMenuItemModel>()
+                {
+                new GrillMenuItemModel() { Id = Guid.NewGuid(), Width = config.GrillSize.Width + 1, Length = config.GrillSize.Height + 1, Name = "Oversized Menu 1", Quantity = 1 }
+                }
+            };
+
+            var planner = new DefaultGrillMenuPlanner(config);
+
+            GrillMenuGrillingPlan plan = planner.Plan(menu);
         }
     }
 }
